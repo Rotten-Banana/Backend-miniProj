d98681edaac4f49d4301460b0cb60b5b12e18865
Souvik Srimani <55970375+Rotten-Banana@users.noreply.github.com>
2021-06-22 14:59:39 +0530

    cookie problem

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
+app.set("trust proxy", 1);
 app.use(express.json({ limit: "25mb" }));
 app.use(cors(corsOptions));
 app.use(
@@ -45,7 +46,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: MAX_AGE,
-      sameSite: "strict",
+      sameSite: "None",
       secure: false,
     },
   })
