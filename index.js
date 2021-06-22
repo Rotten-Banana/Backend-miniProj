daba9bfbd5f32177b3cd157747a0c34d5d472b15
Souvik Srimani <55970375+Rotten-Banana@users.noreply.github.com>
2021-06-22 22:13:34 +0530

    cors origin bug

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ const port = 4000;
 const MAX_AGE = 1000 * 60 * 60 * 5;
 
 const corsOptions = {
-  origin:
-    "https://internal-examination-portal-m462orxar-rotten-banana.vercel.app",
+  origin: "https://internal-examination-portal.vercel.app",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
