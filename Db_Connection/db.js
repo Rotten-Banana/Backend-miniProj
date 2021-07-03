825f8b65849b456d56a7cda7c802a43969b606e7
Souvik Srimani <55970375+Rotten-Banana@users.noreply.github.com>
2021-07-03 12:42:58 +0530

    database changed

--- Db_Connection/db.js
+++ Db_Connection/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: "sql6.freemysqlhosting.net" || "localhost",
+  host: "remotemysql.com",
   port: 3306,
-  user: "sql6419836" || "root",
-  password: "fHmRjyj5g5",
-  database: "sql6419836" || "examportal",
+  user: "cCJHMBZ9GZ",
+  password: "dYCRYg9Nkr",
+  database: "cCJHMBZ9GZ",
 });
 
 connection.connect(function (err) {
